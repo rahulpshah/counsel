6e35205222086d0f2d5971f27f5b6d8928c48d27
Rahul P Shah <rshah5@ncsu.edu>
2016-10-13 21:53:03 -0400

    Matters

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,5 @@
 function convert(epoch_time){
-	
+    
 }
 angular.module('app.controllers', [])
 
@@ -11,56 +11,53 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('mattersCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('mattersCtrl', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, Matters) {
+    $scope.matters = Matters.items;
 
-
-}])
+    // Accordion Code
+    $scope.toggleMatter = function(matter) {
+        if ($scope.isMatterShown(matter)) {
+          $scope.shownMatter = null;
+        } 
+        else 
+        {
+          $scope.shownMatter = event;
+        }
+    };
+    $scope.isMatterShown = function(matter) {
+        return $scope.shownMatter === matter;
+    };
+})
 
 .controller('eventsCtrl',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 
 function ($scope, $stateParams, Events) {
-	//console.log("here's my data", Events.items);
-	console.log(Events.items);
-	console.log(typeof (Events.items));
-	$scope.events = Events.items;
+    $scope.events = Events.items;
 
-	//Code to convert epoch to date time
-	console.log(typeof $scope.events);
-	console.log(Events.items.length);
-	/*
-	for(var i=0;i<$scope.events.length;i++)
-	{
-		console.log("Asd");
-		$scope.events[i].due_date = convert($scope.events[i].due_date)
-		$scope.events[i].start_date = convert($scope.events[i].start_date)
-		$scope.events[i].end_date = convert($scope.events[i].end_date)
-		 
-	}*/
-
-	//Accordion Code
-	$scope.toggleEvent = function(event) {
-    if ($scope.isEventShown(event)) {
-      $scope.shownEvent = null;
-    } else {
-      $scope.shownEvent = event;
+    // Accordion Code
+    $scope.toggleEvent = function(event) {
+        if ($scope.isEventShown(event)) {
+          $scope.shownEvent = null;
+        } 
+        else 
+        {
+          $scope.shownEvent = event;
+        }
+    };
+    $scope.isEventShown = function(event) {
+        return $scope.shownEvent === event;
+    };
+    // Code to convert epoch to date time
+    $scope.convert = function(x){
+        var d = new Date(0);
+        d.setUTCSeconds(x);
+        return d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear();
     }
-  };
-  $scope.isEventShown = function(event) {
-
-    return $scope.shownEvent === event;
-  };
-  $scope.convert = function(x)
-  {
-  	console.log("hello")
-	var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-	d.setUTCSeconds(x);
-	return d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear();
-  }
 })
 
 .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
@@ -75,27 +72,10 @@ function ($scope, $stateParams) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-
-
 }])
 
 .controller('noteCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-
-
 }])
-
-
-
-var wait = function() {
-    var deferred = $q.defer();
-    setTimeout(function() {
-      // Reject 3 out of 10 times to simulate 
-      // some business logic.
-      if (Math.random() > 0.7) deferred.reject('hell');
-      else deferred.resolve('world');
-    }, 1000);
-    return deferred.promise;
-  };
--- www/js/services.js
+++ www/js/services.js
@@ -10,31 +10,51 @@ angular.module('app.services', [])
 .service('Events', ['$firebaseArray',function($firebaseArray){
 
   var config = {
-  apiKey: "AIzaSyCAAITOFc7ivO2qHvDCBdx_fHWEHJj6Yfk",
-  authDomain: "lexisnexishackathon.firebaseapp.com",
-  databaseURL: "https://lexisnexishackathon.firebaseio.com",
-  storageBucket: "lexisnexishackathon.appspot.com",
-  messagingSenderId: "429670093553"
+    apiKey: "AIzaSyCAAITOFc7ivO2qHvDCBdx_fHWEHJj6Yfk",
+    authDomain: "lexisnexishackathon.firebaseapp.com",
+    databaseURL: "https://lexisnexishackathon.firebaseio.com",
+    storageBucket: "lexisnexishackathon.appspot.com",
+    messagingSenderId: "429670093553"
   };
+
   firebase.initializeApp(config);
 
   var ref = firebase.database().ref().child("calendar_event");
 
 
-    // var items = [
-    //   {
-    //     name:"rhaul",
-    //     id:"1"
-    //   },
-    //   {
-    //     name:"moharnab",
-    //     id:"2"
-    //   }
-    // ];
+  var items = $firebaseArray(ref);
+  var events = {
+    'items' : items
+  }
+  ref.child('calendar_event').once('value').then(function(data){
+    //do this whenever data is loaded
+    console.log(data);
+  });
+  return events;
+}])
+.service('Matters', ['$firebaseArray',function($firebaseArray){
+  console.log("ASdasd");
+  var config = {
+    apiKey: "AIzaSyCAAITOFc7ivO2qHvDCBdx_fHWEHJj6Yfk",
+    authDomain: "lexisnexishackathon.firebaseapp.com",
+    databaseURL: "https://lexisnexishackathon.firebaseio.com",
+    storageBucket: "lexisnexishackathon.appspot.com",
+    messagingSenderId: "429670093553"
+  };
 
-    var items = $firebaseArray(ref);
-    var events = {
-      'items' : items
-    }
-    return events;
-}]);
+  firebase.initializeApp(config);
+
+  var ref = firebase.database().ref().child("matter");
+  
+
+  var items = $firebaseArray(ref);
+  console.log(items);
+  var matters = {
+    'items' : items
+  }
+  ref.child('matter').once('value').then(function(data){
+    //do this whenever data is loaded
+    console.log(data);
+  });
+  return matters;
+}])
